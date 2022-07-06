df9c87445f7de7cfc7a00cc35e6c62e8fcec0378
HardRoof <mateus_grandez@hotmail.com>
2022-07-05 18:36:27 -0600

    Added some loaders(pics, fonts, videos)

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,15 @@ module.exports = {
 
   //loaders
   module: {
-    rules: [{test: /\.css$/i, use: ['style-loader', 'css-loader']}]
-  }
+    rules: [
+      {test: /\.css$/i, use: ['style-loader', 'css-loader']},
+      {test: /\.(svg|ico|jpg|jpeg|png|gif|webp)$/i, type:'asset/resource'},
+      {test: /\.(woff|woff2|eot|ttf|otf)$/i, type:'asset/resource'},
+      {test: /\.(mov|mp4)$/, use: [{ loader: 'file-loader', options: {name: '[name].[ext]'}}]},
+    ],
+  },
 
   //plugins
   
 }
+
