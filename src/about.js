de1367476c1850395db5eaec2eb808881076cff5
HardRoof <mateus_grandez@hotmail.com>
2022-07-07 18:33:42 -0600

    Completed about page

--- src/about.js
+++ src/about.js
@@ -1,44 +1,98 @@
 import header from './header.js';
+import companyPicImport from "./img/52f8fb70eab8ead367092e3f.jpeg"
+import coffeePicImport from './img/contact-coffee.jpeg';
 
 function about() {
   const container = document.createElement('div');
+  container.id = "aboutContainer"
 
+  // <-------> //
   const topContent = document.createElement('div');
   container.append(topContent);
   topContent.classList.add('containerClass', 'about');
 
   topContent.append(header());
 
+  // <-------> //
   const titleAbout = document.createElement('h1');
   topContent.append(titleAbout);
   titleAbout.textContent = "ABOUT US";
 
+  // <-------> //
   const bottomContent = document.createElement('div');
   container.append(bottomContent);
 
+   // <-------> //
+  const OurContainer = document.createElement('div');
+  bottomContent.append(OurContainer);
+  OurContainer.classList.add('outer'); 
+
+  const OurCompanyTextDiv = document.createElement('div');
+  OurContainer.append(OurCompanyTextDiv);
+  OurCompanyTextDiv.classList.add('company', 'text'); 
+
   const company = document.createElement('h2');
-  bottomContent.append(company);
+  OurCompanyTextDiv.append(company);
   company.textContent = "OUR COMPANY";
 
   const companyParagraph = document.createElement('p');
-  bottomContent.append(companyParagraph);
-  companyParagraph.textContent = "Dumb Starbucks a coffee experience located in os Angeles California. By adding the word dumb we are legally allowed to use the coveted Starbucks name and logo because we fulfilled the minimum requirements to be considered a parody under US law we have a full menu including all your favorite Starbucks items a delicious selection of pastries";
+  OurCompanyTextDiv.append(companyParagraph);
+  companyParagraph.textContent = "Dumb Starbucks is a coffee experience located in Los Angeles, California. By adding the word 'dumb' we are legally allowed to use the coveted Starbucks name and logo because we fulfilled the minimum requirements to be considered a parody under US law. We have a full menu, including all your favorite Starbucks drinks and a delicious selection of pastries.";
 
   const obs = document.createElement('p');
-  bottomContent.append(obs);
-  obs.textContent = "Ps: This is not a real company or website. It's a parody of a parody";
+  OurCompanyTextDiv.append(obs);
+  obs.textContent = "PS: This is not a real company or website. It's a parody of a parody";
 
+  const companyPic = document.createElement('img');
+  OurContainer.append(companyPic);
+  companyPic.src = companyPicImport;
+  companyPic.alt = 'Store Picture';
+
+// <----------> //
+  const coffeePic = document.createElement('img');
+  OurContainer.append(coffeePic);
+  coffeePic.src = coffeePicImport;
+  coffeePic.alt = 'Store Picture';
+
+  const OurCoffeeTextDiv = document.createElement('div');
+  OurContainer.append(OurCoffeeTextDiv);
+  OurCoffeeTextDiv.classList.add('coffee', 'text'); 
+
+  const coffee = document.createElement('h2');
+  OurCoffeeTextDiv.append(coffee);
+  coffee.textContent = "OUR COFFEE";
+
+  const coffeeParagraph = document.createElement('p');
+  OurCoffeeTextDiv.append(coffeeParagraph);
+  coffeeParagraph.textContent = "It takes many hands to craft the perfect cup of coffee – from the farmers who tend to the red-ripe coffee cherries, to the barista who serves it with care. We are committed to the highest standards of quality and service, creating tasteful experiences to our clients.";
+
+   // <-------> //
   const contactContainer = document.createElement('div');
   bottomContent.append(contactContainer);
+  contactContainer.classList.add('contactContainer'); 
+
+  // <-------> //
+  const contactBox = document.createElement('div');
+  contactContainer.append(contactBox);
   
   const contactTitle = document.createElement('h2');
-  contactContainer.append(contactTitle);
+  contactBox.append(contactTitle);
   contactTitle.textContent = "CONTACT US";
 
   const hours = document.createElement('p');
-  contactContainer.append(hours);
-  hours.textContent = "Monday to Monday \r\n 07:00 AM to 09:00 PM";
+  contactBox.append(hours);
+  hours.textContent = "Monday - Monday \r\n 07:00AM - 09:00PM";
 
+  const address = document.createElement('p');
+  contactBox.append(address);
+  address.textContent = "1802 Hillsomething Ave. \r\n Los Angeles CA 9002X USA ";
+
+  const companyEmail = document.createElement('p');
+  contactBox.append(companyEmail);
+  companyEmail.textContent = "323-5555555 \r\n dumbstarbucks@zmail.com";
+
+
+  // <-------> //
   const form = document.createElement('form');
   contactContainer.appendChild(form);
   form.id = "news"
@@ -46,20 +100,24 @@ function about() {
 
   const label = document.createElement("label");
   const input = document.createElement("input");
+
+  const inputBox = document.createElement('div');
+  
   form.append(label);
-  form.append(input);
+  form.append(inputBox);
+  inputBox.append(input);
 
   input.id = "inputNewsId"
   label.setAttribute('for', input.id);
 
   input.classList.add('inputClass'); 
   input.setAttribute('type', "email");
-  input.placeholder = "EMAIL ADDRESS"
+  input.placeholder = "Email Address"
 
   label.textContent = "Get News & Offers";
 
   const btn = document.createElement("button"); 
-  form.append(btn);
+  inputBox.append(btn);
   btn.innerText = "JOIN →";
 
 
@@ -68,4 +126,3 @@ function about() {
 
 export default about;
 
-// 1802 Hillhurst
